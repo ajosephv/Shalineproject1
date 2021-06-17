0e5168e4f7f03d2769c1ae50a3243aac103410a6
ajosephv <aarun.josh@gmail.com>
2021-06-17 14:07:21 -0500

    story no 4

--- src/shalinejava/class2.java
+++ src/shalinejava/class2.java
@@ -7,8 +7,8 @@ public class class2 {
     System.out.println("hello arun");
     System.out.println("this is shalu");
 		System.out.println("this is shalu");
-		System.out.println("this is shalu");
-		
+		System.out.println("this is amala");
+		System.out.println("hi this is kasandra lancia");
 	}
 
 }
